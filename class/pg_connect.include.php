6ecf37ccb371b86bc33681092756d4cdda7eab60
Alberto Aguilera <a.aguilerax@gmail.com>
2013-03-11 13:11:26 -0600

    Guarda desde sh

--- class/pg_connect.include.php
+++ class/pg_connect.include.php
@@ -9,6 +9,7 @@ class dbOptions{
 		$this->_SITE_ROOT = $_SERVER['DOCUMENT_ROOT']."/SpotPrices/";
 		include($this->_SITE_ROOT."config.php");
 		//sas
+		//blas
 		$this->dbConnection = pg_connect("dbname=$_DB_NAME user=$_DB_USER password=$_DB_PSWD host=$_DB_HOST");
 	}
 	function result_construct($action,$data){
